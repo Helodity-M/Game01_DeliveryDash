10ee63188253d3cd6cf88f15aa94eeefdcb6b1ee
Helodity <228972983+Helodity-M@users.noreply.github.com>
2025-09-10 13:09:42 -0400

    Improvement: Cleanup input code to use GetAxisRaw

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -5,39 +5,15 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 0.3f;
     [SerializeField] float moveSpeed = 0.03f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
     {
-        float move = 0;
-        float steer = 0;
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        float moveAmount = moveSpeed * input.y * Time.deltaTime;
+        //Need to invert input's sign, otherwise A would turn right.
+        float steerAmount = steerSpeed * -input.x * Time.deltaTime;
 
-
-        if (Keyboard.current.wKey.isPressed)
-        {
-            move = 1;
-        }
-        else if (Keyboard.current.sKey.isPressed)
-        {
-            move = -1;
-        }
-
-        if (Keyboard.current.aKey.isPressed)
-        {
-            steer = 1;
-        }
-        else if (Keyboard.current.dKey.isPressed)
-        {
-            steer = -1;
-        }
-
-        float moveAmount = moveSpeed * move * Time.deltaTime;
-        float steerAmount = steerSpeed * steer * Time.deltaTime;
         transform.Rotate(0, 0, steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
