a6f50463598ddcc93b484765e4cc33be8a445e13
Helodity <228972983+Helodity-M@users.noreply.github.com>
2025-09-11 10:55:45 -0400

    Improvement: Revamp movement

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,20 +1,58 @@
+using Unity.Cinemachine;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] float steerSpeed = 0.3f;
     [SerializeField] float moveSpeed = 0.03f;
+    [SerializeField] float moveSteerRatio = -5f;
+
+    [Header("Acceleration")]
+    [SerializeField] AnimationCurve accelerationCurve;
+    [SerializeField] AnimationCurve reverseCurve;
+    [SerializeField] float accelerationSpeed;
+    [SerializeField] float decelerationSpeed;
+    float accelerationTime;
 
     // Update is called once per frame
     void Update()
     {
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        float moveAmount = moveSpeed * input.y * Time.deltaTime;
+        float targetTime = input.y;
+        bool tryingToSlowdown = Mathf.Abs(targetTime - accelerationTime) > Mathf.Abs(targetTime);
+        
+        //Use decelerationSpeed if we are currently trying to slow down
+        float accelerationChange = (tryingToSlowdown ? decelerationSpeed : accelerationSpeed) * Time.deltaTime;
+        accelerationChange = Mathf.Min(accelerationChange, Mathf.Abs(targetTime - accelerationTime));
+        if (targetTime < accelerationTime)
+        {
+            //Moving in reverse
+            accelerationChange *= -1;
+        }
+        accelerationTime += accelerationChange;
+
+        float moveAmount = getMovementCurve() * moveSpeed * Time.deltaTime;
         //Need to invert input's sign, otherwise A would turn right.
-        float steerAmount = steerSpeed * -input.x * Time.deltaTime;
+        float steerAmount = Mathf.Abs(getMovementCurve()) * (steerSpeed + (Mathf.Abs(getMovementCurve()) * moveSteerRatio)) * Time.deltaTime * -input.x;
 
         transform.Rotate(0, 0, steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
+
+    float getMovementCurve()
+    {
+        float curveVal = 0;
+        if (accelerationTime > 0)
+        {
+            curveVal = accelerationCurve.Evaluate(accelerationTime);
+        } 
+        else
+        {
+            curveVal = -reverseCurve.Evaluate(-accelerationTime);
+        }
+
+        return curveVal;
+    }
+
 }
